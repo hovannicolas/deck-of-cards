1b386d1419ba4a6777190a76346b16e7ae246687
Nicolas Hovan <hovan.nicolas@gmail.com>
2019-08-26 10:40:50 -0700

    add Deck instance methods

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-class Card():
+class Card:
     def __init__(self, value, suit, ):
         self.suit = suit
         self.value = value
@@ -7,7 +7,7 @@ class Card():
         return f"{self.value} of {self.suit}"
 
 
-class Deck():
+class Deck:
 
     def __init__(self):
         suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
@@ -18,5 +18,32 @@ class Deck():
                 self.cards.append(Card(value, suit))
         print(self.cards)
 
-Deck()
+    def __repr__(self):
+        return f"Deck of {len(self.cards)} cards."
+
+    def count(self):
+        return len(self.cards)
+
+    def _deal(self, num):
+        count = self.count()
+        if count == 0:
+            raise ValueError("All cards have been dealt")
+        actual = min(num, count)
+        cards = self.cards[-actual:]
+        self.cards = self.cards[:-actual]
+
+    def shuffle(self):
+        if self.count() < 52:
+            raise ValueError("Only full decks can be shuffled")
+        shuffle(self.cards)
+        return self.cards
+
+    def deal_card(self):
+        return self._deal(1)[0]
+
+    def deal_hand(self, hand_size):
+        return self._deal(hand_size)
+
+
+
 
